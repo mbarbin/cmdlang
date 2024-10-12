02a852923260adb4d9a7ce775352681bc7e3a88a
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-10-12 18:50:11 +0200

    Update tutorial to use cmdlang-to-cmdliner

--- doc/docs/tutorials/getting-started/bin/main.ml
+++ doc/docs/tutorials/getting-started/bin/main.ml
@@ -1,6 +1,8 @@
 let () =
-  Cmdlang_cmdliner_runner.run
-    Getting_started.cmd
-    ~name:"my-calculator"
-    ~version:"%%VERSION%%"
+  Cmdliner.Cmd.eval
+    (Cmdlang_to_cmdliner.Translate.command
+       Getting_started.cmd
+       ~name:"my-calculator"
+       ~version:"%%VERSION%%")
+  |> Stdlib.exit
 ;;
