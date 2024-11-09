0364305fe3aef37d226fab8c494b20cb0c5ef3f1
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-11-10 00:11:11 +0100

    pp-log.err and .cli renames

--- test/cram/bin/cmdliner/main_cmdliner.ml
+++ test/cram/bin/cmdliner/main_cmdliner.ml
@@ -1,6 +1,8 @@
 let () =
-  Cmdlang_cmdliner_runner.run
-    Cram_test_command.Cmd.main
-    ~name:Sys.argv.(0)
-    ~version:"%%VERSION%%"
+  Cmdliner.Cmd.eval
+    (Cmdlang_to_cmdliner.Translate.command
+       Cram_test_command.Cmd.main
+       ~name:Sys.argv.(0)
+       ~version:"%%VERSION%%")
+  |> Stdlib.exit
 ;;
