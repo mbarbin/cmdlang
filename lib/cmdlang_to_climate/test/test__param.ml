5c274f1c704083fa37eec3e93c6728f191c3fa3a
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-11-29 19:01:29 +0100

    Add tests

--- lib/cmdlang_to_climate/test/test__param.ml
+++ lib/cmdlang_to_climate/test/test__param.ml
@@ -17,3 +17,53 @@ let%expect_test "param" =
     |}];
   ()
 ;;
+
+module Color = struct
+  type t =
+    | Red
+    | Green
+    | Blue
+
+  let all = [ Red; Green; Blue ]
+
+  let to_string = function
+    | Red -> "red"
+    | Green -> "green"
+    | Blue -> "blue"
+  ;;
+end
+
+let%expect_test "enumerated" =
+  let conv =
+    Cmdlang_to_climate.Translate.param (Command.Param.enumerated (module Color))
+  in
+  List.iter Color.all ~f:(fun color -> Stdlib.Format.printf "%a\n" conv.print color);
+  [%expect {|
+    red
+    green
+    blue
+    |}];
+  (* Here we characterize what happens when [all] doesn't include all
+     inhabitants of the enumerated [t]. *)
+  let module Missing_color = struct
+    include Color
+
+    let all = [ Red; Green ]
+  end
+  in
+  let conv =
+    Cmdlang_to_climate.Translate.param (Command.Param.enumerated (module Missing_color))
+  in
+  List.iter Missing_color.all ~f:(fun color ->
+    Stdlib.Format.printf "%a\n" conv.print color);
+  [%expect {|
+    red
+    green
+    |}];
+  (match Stdlib.Format.printf "%a\n" conv.print Blue with
+   | () -> assert false
+   | exception Climate.Spec_error.E e -> print_endline (Climate.Spec_error.to_string e));
+  [%expect
+    {| Attempted to format an enum value as a string but the value does not appear in the enum declaration. Valid names for this enum are: red green |}];
+  ()
+;;
