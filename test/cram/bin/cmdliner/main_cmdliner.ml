b3b90b8b75d4d29de48d3bb113ff4be86f0c025a
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-24 12:56:05 +0100

    Add coverage annotation for post-installed path

--- test/cram/bin/cmdliner/main_cmdliner.ml
+++ test/cram/bin/cmdliner/main_cmdliner.ml
@@ -7,7 +7,7 @@
 let version =
   match Build_info.V1.version () with
   | None -> "n/a"
-  | Some v -> Build_info.V1.Version.to_string v
+  | Some v -> Build_info.V1.Version.to_string v [@coverage off]
 ;;
 
 let () =
